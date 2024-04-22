e38c4b9ae3b60c0fdea5849903dc87337225f07f
Afrae El Fatuaki <afrae@elfatuaki.com>
2024-04-22 18:44:06 +0200

    adding helmet and cors to commonMiddleware for security

--- server/middleware/commonMiddleware.mjs
+++ server/middleware/commonMiddleware.mjs
@@ -1,8 +1,16 @@
 import express from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 
 import { isDevelopmentEnvironment } from '../utils/environment.mjs';
 
 export function applycommonMiddleware(app) {
+  // Provide HTTP headers security
+  app.use(helmet());
+
+  // Enable Cross-Origin Resource Sharing
+  app.use(cors());
+
   // Parse incoming requests with URL-encoded payloads
   app.use(express.urlencoded({ extended: false }));
 
