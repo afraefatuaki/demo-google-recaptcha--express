fbb99a4c4c5c8065e9cf6a6ba0ddad38e19ce6e7
Afrae El Fatuaki <afrae@elfatuaki.com>
2024-05-04 23:31:07 +0200

    added jest testing setup

---
+++ server/test/utils/environment.test.js
@@ -0,0 +1,14 @@
+const { describe, expect, test } = require('@jest/globals');
+const { getEnvironment } = require('../../utils/environment');
+
+describe('get current environment', () => {
+  test('should return the correct environment', () => {
+    process.env.NODE_ENV = 'development';
+    expect(getEnvironment()).toBe('development');
+  });
+
+  test('should return undefined if NODE_ENV is not set', () => {
+    delete process.env.NODE_ENV;
+    expect(getEnvironment()).toBeUndefined();
+  });
+});
