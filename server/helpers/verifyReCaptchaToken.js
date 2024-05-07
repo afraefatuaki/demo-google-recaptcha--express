23af94d78afcb3e689016a72bcfe84f9d4c9ae7c
Afrae El Fatuaki <afrae@elfatuaki.com>
2024-05-07 20:00:17 +0200

    Added function to verify reCaptcha token

---
+++ server/helpers/verifyReCaptchaToken.js
@@ -0,0 +1,10 @@
+const verifyReCaptchaToken = async (token) => {
+  const response = await fetch(
+    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`,
+    { method: "POST" },
+  );
+
+  return await response.json();
+};
+
+module.exports = verifyReCaptchaToken;
